ae537ddc675fbb82f7fe6483d71912f5aea581a5
lzwaiwai <lzwai3230@foxmail.com>
2017-01-05 15:18:59 +0800

    delete all

--- h5/components/Flexbox-item.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="vux-flexbox-item" :style="{marginLeft: $parent.supported ? $parent.marginLeft + 'px' : 0}">
-    <slot></slot>
-  </div>
-</template>
--- h5/components/UploadImg/readme.md
+++
@@ -1,2 +0,0 @@
-
-    <Upload-img :max-num=4></Upload-img>            #引入组件 （max-num）最多上传数
